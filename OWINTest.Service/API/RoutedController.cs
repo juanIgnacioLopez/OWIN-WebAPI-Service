0e8c3b5882af278200678450110c328e35feb45c
Juan Ignacio Lopez <jilopez@outlook.com>
2020-01-09 17:35:33 -0300

    Test getHealthCheck

--- OWINTest.Service/API/RoutedController.cs
+++ OWINTest.Service/API/RoutedController.cs
@@ -1,19 +1,46 @@
-﻿using System;
+﻿using OWINTest.Service.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 
-namespace OWINTest.Service.API
-{
+namespace OWINTest.Service.API {
     [RoutePrefix("api/testing")]
-    public class RoutedController : ApiController
-    {
+    public class RoutedController : ApiController {
         [Route("getall")]
-        public IEnumerable<string> GetAllItems()
-        {
-            return new string[] { "value1", "value2" };
+        public IEnumerable<string> GetAllItems() {
+            return new string [] { "value1","value2" };
+        }
+        [Route("check")]
+        public string [] Check() {
+            return Data();
+        }
+        [Route("healthcheck")]
+        public healthCheck GetHealthCheck() {
+            var data = Data();
+            healthCheck result = new healthCheck(data [0],data [1]);
+            //return ToJSON(result);
+            return result;
+        }
+        public static string ToJSON(object obj) {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            return serializer.Serialize(new {
+                Estado =
+                obj
+            });
+        }
+
+        private string [] Data() {
+            ServiceController sc = new ServiceController("OWINServiceTest");
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            string [] result = { sc.Status.ToString(),response.StatusCode.ToString() };
+            return result;
         }
     }
 }
---
+++ OWINTest.Service/Models/healthCheck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWINTest.Service.Models {
+    public class healthCheck {
+        public string servicio;
+        public string request;
+
+        public healthCheck(string servicio,string request) {
+            this.servicio = servicio;
+            this.request = request;
+        }
+    }
+}
